12b6c4cd90cd9b059c7470310ca5c5749a877c85
cartolari <bruno.cartolari@gmail.com>
2017-09-12 19:48:18 -0300

    Initial commit

---
+++ main.tf
@@ -0,0 +1,67 @@
+resource "aws_security_group" "rds" {
+  name   = "rds-${var.identifier}"
+  vpc_id = "${var.vpc_id}"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "rds-${var.identifier}"
+  }
+}
+
+resource "aws_rds_cluster" "default" {
+  apply_immediately            = "${var.apply_immediately}"
+  availability_zones           = "${var.availability_zones}"
+  backup_retention_period      = "${var.backup_retention_period_in_days}"
+  cluster_identifier           = "${var.identifier}"
+  count                        = "${lookup(map("0", "0"), var.aurora_instance_count, 1)}"
+  database_name                = "${var.db_name}"
+  db_subnet_group_name         = "${aws_db_subnet_group.rds.id}"
+  master_password              = "${var.db_password}"
+  master_username              = "${var.db_username}"
+  port                         = "${var.db_port}"
+  preferred_backup_window      = "${var.backup_window}"
+  preferred_maintenance_window = "${var.maintenance_window}"
+  vpc_security_group_ids       = ["${aws_security_group.rds.id}"]
+}
+
+resource "aws_rds_cluster_instance" "cluster_instances" {
+  cluster_identifier   = "${aws_rds_cluster.default.0.id}"
+  count                = "${var.aurora_instance_count}"
+  db_subnet_group_name = "${aws_db_subnet_group.rds.id}"
+  identifier           = "${var.identifier}-${count.index}"
+  instance_class       = "${var.instance_class}"
+  publicly_accessible  = "${var.publicly_accessible}"
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage       = "${var.rds_storage}"
+  apply_immediately       = "${var.apply_immediately}"
+  backup_retention_period = "${var.backup_retention_period_in_days}"
+  backup_window           = "${var.backup_window}"
+  count                   = "${var.rds_instance_count}"
+  db_subnet_group_name    = "${aws_db_subnet_group.rds.id}"
+  engine                  = "${var.rds_engine}"
+  engine_version          = "${var.rds_version}"
+  identifier              = "${var.identifier}"
+  instance_class          = "${var.instance_class}"
+  maintenance_window      = "${var.maintenance_window}"
+  multi_az                = "${var.rds_multi_az}"
+  name                    = "${var.db_name}"
+  password                = "${var.db_password}"
+  publicly_accessible     = "${var.publicly_accessible}"
+  port                    = "${var.db_port}"
+  storage_type            = "${var.rds_storage_type}"
+  username                = "${var.db_username}"
+  vpc_security_group_ids  = ["${aws_security_group.rds.id}"]
+}
+
+resource "aws_db_subnet_group" "rds" {
+  name       = "${var.identifier}"
+  subnet_ids = ["${var.subnet_ids}"]
+}
---
+++ variables.tf
@@ -0,0 +1,65 @@
+variable "apply_immediately" {}
+
+variable "aurora_instance_count" {
+  description = "Number of db nodes, if this is specified rds_instance_count should be zero."
+}
+
+variable "availability_zones" {
+  type = "list"
+}
+
+variable "backup_retention_period_in_days" {}
+
+variable "backup_window" {}
+
+variable "db_name" {
+  description = "Initial db name for RDS to create"
+}
+
+variable "db_password" {}
+
+variable "db_port" {}
+
+variable "db_username" {}
+
+variable "identifier" {
+  description = "Identifier to show in RDS API"
+}
+
+variable "instance_class" {
+  description = "RDS instance type (db.t2.small, db.r3.large, etc)"
+}
+
+variable "maintenance_window" {}
+
+variable "publicly_accessible" {
+  default = false
+}
+
+variable "rds_engine" {
+  description = "DB Engine to use in RDS (Postgres, Mysql, etc) unless using AWS Aurora"
+}
+
+variable "rds_instance_count" {
+  description = "Number of db nodes, if this is specified it should be 1 and aurora_instance_count should be zero."
+}
+
+variable "rds_multi_az" {
+  description = "If the instance is multi-AZ. Used only for non aurora instances."
+}
+
+variable "rds_storage" {
+  description = "Size of the database disk (used only in standard RDS, not Aurora)"
+}
+
+variable "rds_storage_type" {
+  description = "Storage type standard, gp2, io1 (used only in standard RDS, not Aurora)"
+}
+
+variable "rds_version" {}
+
+variable "subnet_ids" {
+  type = "list"
+}
+
+variable "vpc_id" {}
